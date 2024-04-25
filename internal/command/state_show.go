96396662c9eb7ce235b8f56d2178af905eb2726c
siddharthasonker95 <158144589+siddharthasonker95@users.noreply.github.com>
2024-04-25 20:05:29 +0530

    added show sensitive support for state show
    Signed-off-by: siddharthasonker95 <158144589+siddharthasonker95@users.noreply.github.com>

--- internal/command/state_show.go
+++ internal/command/state_show.go
@@ -32,14 +32,15 @@ type StateShowCommand struct {
 func (c *StateShowCommand) Run(args []string) int {
 	args = c.Meta.process(args)
 	cmdFlags := c.Meta.defaultFlagSet("state show")
+	cmdFlags.BoolVar(&c.Meta.showSensitive, "show-sensitive", false, "displays sensitive values")
 	cmdFlags.StringVar(&c.Meta.statePath, "state", "", "path")
 	if err := cmdFlags.Parse(args); err != nil {
 		c.Streams.Eprintf("Error parsing command-line flags: %s\n", err.Error())
 		return 1
 	}
 	args = cmdFlags.Args()
-	if len(args) != 1 {
-		c.Streams.Eprint("Exactly one argument expected.\n")
+	if len(args) == 0 {
+		c.Streams.Eprint("Atlest one argument expected.\n")
 		return cli.RunResultHelp
 	}
 
@@ -181,6 +182,16 @@ func (c *StateShowCommand) Run(args []string) int {
 		RunningInAutomation: c.RunningInAutomation,
 	}
 
+	// If the "-show-sensitive" argument is provided, reset the value of
+	// resource.SensitiveValues to display the sensitive values.
+	if c.Meta.showSensitive {
+		for index, resource := range root.Resources {
+			if len(resource.SensitiveValues) > 0 {
+				jsonstate.SetSensitive(root.Resources, nil, index)
+			}
+		}
+	}
+
 	renderer.RenderHumanState(jstate)
 	return 0
 }
@@ -201,6 +212,7 @@ Options:
                       up OpenTofu-managed resources. By default it will
                       use the state "terraform.tfstate" if it exists.
 
+  -show-sensitive  If specified, sensitive values will be displayed.
 `
 	return strings.TrimSpace(helpText)
 }
